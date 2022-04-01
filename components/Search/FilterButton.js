5ffeeec0fbabb9a1685a406c6809a506c29242c6
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2022-04-02 04:35:06 +0800

    added filter button

--- components/Search/FilterButton.js
+++ components/Search/FilterButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './FilterButton.module.css';
 
 const FilterButton = () => {
-	return <div>FilterButton</div>;
+	return (
+		<div className={classes.container}>
+			<button>Filter results</button>
+		</div>
+	);
 };
 
 export default FilterButton;
