e48499404c556cfa1af1ae538bde81c9615e8d7d
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2022-04-02 01:10:13 +0800

    added redux and job pages files

---
+++ components/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import FilterButton from './FilterButton';
+
+const Search = () => {
+	return (
+		<div>
+			<SearchBar />
+			<FilterButton />
+		</div>
+	);
+};
+
+export default Search;
